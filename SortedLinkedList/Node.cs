d6b71b4f27ea8b37c34a513369c135e255aa326d
bishtgovind30 <bisht.govind1988@gmail.com>
2021-07-14 21:05:46 +0530

    Node for Sorted linked ist

---
+++ SortedLinkedList/Node.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DSA.SortedLinkedList
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int value)
+        {
+            data=value;
+        }
+    }
+}
